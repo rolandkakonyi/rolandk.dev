a9575e97903af7541967281a58f4e391ab32ba63
Roland Kakonyi <rolandkakonyi@gmail.com>
2026-02-10 08:23:48 +0000

    Inline stylesheets to reduce render-blocking CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rolandk.dev',
+	build: {
+		inlineStylesheets: 'always',
+	},
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		rehypePlugins: [
